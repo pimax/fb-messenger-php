e2cafdb57a7e33f2002d03e4f11c191de0ee1e32
pimax <m.pinyugin@gmail.com>
2016-04-20 23:05:09 +0300

    added ImageMessage

---
+++ Messages/ImageMessage.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace pimax\Messages;
+
+
+/**
+ * Class ImageMessage
+ *
+ * @package pimax\Messages
+ */
+class ImageMessage
+{
+    /**
+     * @var integer|null
+     */
+    protected $recipient = null;
+
+    /**
+     * @var string
+     */
+    protected $text = null;
+
+    /**
+     * Message constructor.
+     *
+     * @param $recipient
+     * @param $file Web Url or local file with @ prefix
+     */
+    public function __construct($recipient, $file)
+    {
+        $this->recipient = $recipient;
+        $this->text = $file;
+
+    }
+
+    /**
+     * Get message data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        $res = [
+            'recipient' =>  [
+                'id' => $this->recipient
+            ]
+        ];
+
+        if (strpos($this->text, '@') === 0) {
+            // Local file
+
+            $res['message'] = [
+                'type' => 'image',
+                'payload' => []
+            ];
+
+            $res['filedata'] = $this->text;
+
+        } else {
+            // Url
+
+            $res['message'] = [
+                'type' => 'image',
+                'payload' => [
+                    'url' => $this->text
+                ]
+            ];
+        }
+
+
+        return $res;
+    }
+}
