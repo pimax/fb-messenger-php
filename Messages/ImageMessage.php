1c00260a68b533118604e84aac83e45739ef9ff5
Paul Radt <paul@spinninrecords.nl>
2016-07-11 10:05:55 +0200

    Introduce attachment class

--- Messages/ImageMessage.php
+++ Messages/ImageMessage.php
@@ -2,37 +2,13 @@
 
 namespace pimax\Messages;
 
-
 /**
  * Class ImageMessage
  *
  * @package pimax\Messages
  */
-class ImageMessage
+class ImageMessage extends Message
 {
-    /**
-     * @var null|string
-     */
-    protected $recipient = null;
-
-    /**
-     * @var null|string
-     */
-    protected $text = null;
-
-    /**
-     * Message constructor.
-     *
-     * @param string $recipient
-     * @param string $file Web Url or local file with @ prefix
-     */
-    public function __construct($recipient, $file)
-    {
-        $this->recipient = $recipient;
-        $this->text = $file;
-
-    }
-
     /**
      * Get message data
      *
@@ -46,33 +22,15 @@ class ImageMessage
             ]
         ];
 
+        $attachment = new Attachment(Attachment::TYPE_IMAGE);
+
         if (strpos($this->text, 'http://') === 0 || strpos($this->text, 'https://') === 0) {
-
-            // Url
-
-            $res['message'] = [
-                'attachment' => [
-                    'type' => 'image',
-                    'payload' => [
-                        'url' => $this->text
-                    ]
-                ]
-            ];
-
+            $attachment->setPayload(array('url' => $this->text));
         } else {
-
-            // Local file
-
-            $res['message'] = [
-                'attachment' => [
-                    'type' => 'image',
-                    'payload' => []
-                ]
-
-            ];
-
-            $res['filedata'] = $this->getCurlValue($this->text, mime_content_type($this->text), basename($this->text));
+            $attachment->setFileData($this->getCurlValue($this->text, mime_content_type($this->text), basename($this->text)));
         }
+
+        $res['message'] = $attachment->getData();
 
         return $res;
     }
