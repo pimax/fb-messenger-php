eaa6a83f5c2aed7e2c4245758a99c7baf445d334
pimax <m.pinyugin@gmail.com>
2016-08-11 03:46:26 +0300

    local files

--- Messages/Message.php
+++ Messages/Message.php
@@ -60,9 +60,9 @@ class Message
     {
         // PHP 5.5 introduced a CurlFile object that deprecates the old @filename syntax
         // See: https://wiki.php.net/rfc/curl-file-upload
-        if (function_exists('curl_file_create')) {
-            return curl_file_create($filename, $contentType, $postname);
-        }
+        //if (function_exists('curl_file_create')) {
+        //    return curl_file_create($filename, $contentType, $postname);
+        //}
 
         // Use the old style if using an older version of PHP
         $value = "@{$this->filename};filename=" . $postname;
