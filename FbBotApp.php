f4113b1ff03535fba4b5f16d9e37aa82e9ec4c47
Fabian <fabian.witt@t-online.de>
2017-01-03 10:22:32 +0100

    add senderAction function to bot class

--- FbBotApp.php
+++ FbBotApp.php
@@ -4,6 +4,7 @@ namespace hvointern\FbMessenger;
 
 use hvointern\FbMessenger\Messages\Message;
 use hvointern\FbMessenger\Messages\MessageButton;
+use hvointern\FbMessenger\Messages\SenderAction;
 
 class FbBotApp
 {
@@ -52,6 +53,17 @@ class FbBotApp
     public function send($message)
     {
         return $this->call('me/messages', $message->getData());
+    }
+
+    /**
+     * Sender Action
+     *
+     * @param SenderAction $action
+     * @return array
+     */
+    public function senderAction($action)
+    {
+        return $this->call('me/messages', $action->getData());
     }
 
     /**
