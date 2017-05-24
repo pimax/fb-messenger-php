d06c3fdaf2983ea9dac8b46993a8bfd9d3378b21
Jason Engage <jason@engageify.com>
2017-05-24 15:13:59 +0700

    updated userprofile fields - is_payment_enabled, last_ad_referral

--- FbBotApp.php
+++ FbBotApp.php
@@ -78,7 +78,7 @@ class FbBotApp
      * @param string $fields
      * @return UserProfile
      */
-    public function userProfile($id, $fields = 'first_name,last_name,profile_pic,locale,timezone,gender')
+    public function userProfile($id, $fields = 'first_name,last_name,profile_pic,locale,timezone,gender,is_payment_enabled,last_ad_referral')
     {
         return new UserProfile($this->call($id, [
             'fields' => $fields
