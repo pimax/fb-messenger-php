808b087867520acba47901acdf8c3796215ea28b
Fabian Witt <fabian.witt@t-online.de>
2019-05-05 11:46:55 +0200

    add tests

--- tests/TestCase/Messages/AccountLinkTest.php
+++ tests/TestCase/Messages/AccountLinkTest.php
@@ -10,5 +10,33 @@ use pimax\Test\TestCase\AbstractTestCase;
  */
 class AccountLinkTest extends AbstractTestCase
 {
+    public function testAccountLink(): void
+    {
+        $expectedJson = file_get_contents(__DIR__ . '/../../Mocks/Messages/AccountLink/link.json');
 
+        $accountLink = new AccountLink(
+            $title='title', 
+            $subtitle='subtitle', 
+            $url='https://www.example.com/oauth/authorize',
+            $image_url='https://www.facebook.com/images/fb_icon_325x325.png', 
+            $logout=false
+        );
+
+        $this->assertJsonStringEqualsJsonString($expectedJson, json_encode($accountLink->getData()));
+    }
+
+    public function testAccountUnlink(): void
+    {
+        $expectedJson = file_get_contents(__DIR__ . '/../../Mocks/Messages/AccountLink/unlink.json');
+
+        $accountUnlink = new AccountLink(
+            $title='title', 
+            $subtitle='subtitle', 
+            $url='',
+            $image_url='https://www.facebook.com/images/fb_icon_325x325.png', 
+            $logout=true
+        );
+
+        $this->assertJsonStringEqualsJsonString($expectedJson, json_encode($accountUnlink->getData()));
+    }
 }
